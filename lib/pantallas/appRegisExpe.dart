5495db569a7c459b1f6303b9a3afb54ca632ca6c
Patas-Iturralde <93343192+Patas-Iturralde@users.noreply.github.com>
2023-09-19 11:24:04 -0500

    Disenio final

--- lib/pantallas/appRegisExpe.dart
+++ lib/pantallas/appRegisExpe.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gestor_app/modelos/denominacion.dart';
 import 'package:gestor_app/pantallas/appcabeceras.dart';
 import 'package:gestor_app/pantallas/provaider/appDirecciones.dart';
 import 'package:gestor_app/servicios/appvalidarAcceso.dart';
 import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../ventasAlerta/alertaProgreso.dart';
 
@@ -19,10 +22,12 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
   final correo = TextEditingController();
   final celu = TextEditingController();
   final msgSoli = TextEditingController();
-  final denominacion = TextEditingController();
+  final picker = ImagePicker();
+  File? imagen; // Cambio a File? para permitir null
+  var pickedfile;
+  String selectedItemId = 'Seleccione una opción';
 
   bool blockCedula = true;
-  String selectedItemId = ''; // Inicializamos selectedItemId aquí
 
   @override
   void initState() {
@@ -38,9 +43,7 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
     setState(() {
       list = data;
       if (list.isNotEmpty) {
-        selectedItemId = list[0]
-            .id
-            .toString(); // Establecer el primer valor como predeterminado
+        selectedItemId = list[0].id.toString();
       }
     });
   }
@@ -51,7 +54,6 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
   }
 
   bool _validatePhoneNumber(String phoneNumber) {
-    // Expresión regular para validar números de teléfono (solo números)
     final phoneRegex = RegExp(r'^\d+$');
     return phoneRegex.hasMatch(phoneNumber);
   }
@@ -60,11 +62,50 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
     return message.isNotEmpty;
   }
 
+  Future<void> _takePicture() async {
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.camera, imageQuality: 1);
+    if (pickedFile != null) {
+      setState(() {
+        imagen = File(pickedFile.path);
+      });
+      // Convierte la imagen en una cadena Base64
+      String? base64Image = imageToBase64(imagen);
+
+      // Ahora puedes usar base64Image según tus necesidades
+      print("Imagen en Base64: $base64Image");
+    }
+  }
+
+  Future<void> _selectPicture() async {
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.gallery, imageQuality: 1);
+    if (pickedFile != null) {
+      setState(() {
+        imagen = File(pickedFile.path);
+      });
+      // Convierte la imagen en una cadena Base64
+      String? base64Image = imageToBase64(imagen);
+
+      // Ahora puedes usar base64Image según tus necesidades
+      print("Imagen en Base64: $base64Image");
+    }
+  }
+
+  String? imageToBase64(File? imageFile) {
+    if (imageFile == null) {
+      return null; // Devuelve null si no hay imagen
+    }
+    List<int> imageBytes = imageFile.readAsBytesSync();
+    String base64Image = base64Encode(imageBytes);
+    return base64Image;
+  }
+
   @override
   Widget build(BuildContext context) {
     final appvalidarAcceso =
         Provider.of<AppvalidarAcceso>(context, listen: false);
-    appvalidarAcceso.denominacion();
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -112,38 +153,46 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
                   padding: EdgeInsets.all(12.0),
                 ),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  child: ElevatedButton.icon(
-                    onPressed: () async {
-                      alertaProgreso().progreso(context);
-                      final datVerificador =
-                          await AppvalidarAcceso().validarCedula(cedula.text);
-                      final verificador = datVerificador[0].codigo;
-                      final nombComple = datVerificador[0].completo;
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Column(
+                      children: [
+                        ElevatedButton.icon(
+                          onPressed: () async {
+                            alertaProgreso().progreso(context);
+                            final datVerificador = await AppvalidarAcceso()
+                                .validarCedula(cedula.text);
+                            final verificador = datVerificador[0].codigo;
+                            final nombComple = datVerificador[0].completo;
 
-                      if (verificador == 0) {
-                        Navigator.of(context).pop();
+                            if (verificador == 0) {
+                              Navigator.of(context).pop();
 
-                        alertaProgreso().alertas(context, 'Cedula Incorrecta',
-                            'verifique Porfavor', 1);
-                      } else {
-                        blockCedula = false;
-                        Navigator.of(context).pop();
-                        context.read<appdireciones>().asignarNomb(nombComple);
-                      }
-                    },
-                    icon: const Icon(Icons.fingerprint),
-                    label: const Text(
-                      "Validar",
-                      style: TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 16),
-                      backgroundColor: Colors.lightBlue[900],
-                    ),
-                  ),
-                ),
+                              alertaProgreso().alertas(context,
+                                  'Cedula Incorrecta', 'Verifique Porfavor', 1);
+                            } else {
+                              blockCedula = false;
+                              Navigator.of(context).pop();
+                              context
+                                  .read<appdireciones>()
+                                  .asignarNomb(nombComple);
+                            }
+                          },
+                          icon: const Icon(Icons.fingerprint),
+                          label: const Text(
+                            "Validar",
+                            style: TextStyle(fontSize: 14, color: Colors.white),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 16),
+                            backgroundColor: Colors.lightBlue[900],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    )),
               ],
             ),
             const Divider(
@@ -176,7 +225,12 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
               ],
             ),
             const Divider(
-              height: 80.0,
+              height: 20.0,
+              color: Colors.transparent,
+            ),
+            imagen == null ? Center() : Image.file(imagen!, height: 200),
+            const Divider(
+              height: 20.0,
               color: Colors.transparent,
             ),
             Column(
@@ -241,7 +295,7 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
                   controller: msgSoli,
                   decoration: const InputDecoration(
                     isDense: true,
-                    contentPadding: EdgeInsets.all(40),
+                    contentPadding: EdgeInsets.all(20),
                     border: OutlineInputBorder(),
                     labelText: 'Mensaje',
                     labelStyle: TextStyle(
@@ -263,23 +317,157 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
               height: 10.0,
               color: Colors.transparent,
             ),
-            Column(
+            Row(
               children: [
-                DropdownButton<String>(
+                Container(
+                  width: 250, // Establece el ancho deseado
+                  padding: EdgeInsets.all(10), // Agrega relleno para los bordes
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black), // Agrega bordes
+                    borderRadius:
+                        BorderRadius.circular(5), // Aplica bordes redondeados
+                  ),
+                  child: Center(
+                    child: DropdownButton<String>(
                   value: selectedItemId,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedItemId = newValue!;
-                      print(selectedItemId);
-                    });
-                  },
+  onChanged: (String? newValue) {
+    setState(() {
+      selectedItemId = newValue!;
+      print(selectedItemId);
+    });
+  },
+                  underline: Container(), // Quita la línea debajo del texto
                   items: list.map((RespuestaDenominacion item) {
                     return DropdownMenuItem<String>(
                       value: item.id,
-                      child: Text(item.denominacion),
+                      child: Center(
+                        child: Text(
+                          item.denominacion,
+                          style: TextStyle(
+                            decoration: TextDecoration
+                                .none, // Quita la decoración de texto (en este caso, la línea debajo del texto)
+                          ),
+                        ),
+                      ),
                     );
                   }).toList(),
-                )
+                ),
+                  ),
+                ),
+                const VerticalDivider(
+                  width: 10.0,
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          contentPadding: EdgeInsets.all(0),
+                          content: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                InkWell(
+                                  onTap: () async {
+                                    _takePicture();
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(20),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                width: 1, color: Colors.grey))),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                            child: Text(
+                                          "Tomar una foto",
+                                          style: TextStyle(fontSize: 16),
+                                        )),
+                                        Icon(Icons.camera_alt,
+                                            color: Colors.blue)
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    _selectPicture();
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(20),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                            child: Text(
+                                          "Seleccionar una foto",
+                                          style: TextStyle(fontSize: 16),
+                                        )),
+                                        Icon(Icons.image, color: Colors.blue)
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    imagen = null;
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(20),
+                                    decoration:
+                                        BoxDecoration(color: Colors.deepOrange),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                            child: Text(
+                                          "Quitar foto",
+                                          style: TextStyle(fontSize: 16),
+                                        )),
+                                        Icon(Icons.image, color: Colors.blue)
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.all(20),
+                                    decoration:
+                                        BoxDecoration(color: Colors.red),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                            child: Text(
+                                          "Atras",
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.white),
+                                          textAlign: TextAlign.center,
+                                        )),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  icon: const Icon(Icons.camera),
+                  label: Text("Foto"),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 22, vertical: 22),
+                    backgroundColor: Colors.lightBlue[900],
+                  ),
+                ),
               ],
             ),
             const Divider(
@@ -289,21 +477,31 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
             Row(
               children: [
                 const Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: EdgeInsets.all(11.0),
                 ),
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.25,
                   child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.border_color),
+                    onPressed: () {
+                      cedula.clear();
+                      blockCedula = true;
+                      context.read<appdireciones>().asignarNomb('');
+                      imagen = null;
+                      correo.clear();
+                      celu.clear();
+                      msgSoli.clear();
+                      
+                      // Establecer en "predeterminado"
+                    },
+                    icon: const Icon(Icons.delete_outlined),
                     label: const Text(
-                      "Registro",
-                      style: TextStyle(fontSize: 10, color: Colors.white),
+                      "Limpiar",
+                      style: TextStyle(fontSize: 12, color: Colors.white),
                     ),
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 15),
-                      backgroundColor: Colors.lightBlue[900],
+                      backgroundColor: Colors.deepOrangeAccent,
                     ),
                   ),
                 ),
@@ -334,9 +532,11 @@ class _AppRegisExpeState extends State<AppRegisExpe> {
                             cedula.clear();
                             blockCedula = true;
                             context.read<appdireciones>().asignarNomb('');
+                            imagen = null;
                             correo.clear();
                             celu.clear();
                             msgSoli.clear();
+                            
                           });
                         } else {
                           alertaProgreso().alertas(

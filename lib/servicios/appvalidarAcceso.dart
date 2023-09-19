2a8a804f41348f677a7ce10475f0269903f3914e
L-S16 <sorialuwis@gmail.com>
2023-09-19 08:40:33 -0500

    CAMBIO APPI

--- lib/servicios/appvalidarAcceso.dart
+++ lib/servicios/appvalidarAcceso.dart
@@ -58,7 +58,7 @@ class AppvalidarAcceso extends ChangeNotifier {
   Future<List<Respuesta>> regisExpe(
       solicitante, pedido, fono, mail, codigo) async {
     final http.Response respuestaV = await http.get(Uri.parse(
-        'http://186.46.158.7/AppattCiu/Regis_Solicitudes.php?remitente=$solicitante&detaPedido=$pedido&celu=$fono&correo=$mail'));
+        'http://186.46.158.7/AppattCiu/Regis_Solicitudes.php?remitente=$solicitante&detaPedido=$pedido&celu=$fono&correo=$mail,&codtpo=$codigo'));
     List<Respuesta> datVali = [];
     if (respuestaV.statusCode == 200) {
       String cuerpo = utf8.decode(respuestaV.bodyBytes);
